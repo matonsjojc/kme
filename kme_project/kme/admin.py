74858f8dfe3ab2ca1227bce63d542a2fe92996a5
matonsjojc <matevz.podlipnik@gmail.com>
2017-06-06 19:20:36 +0200

    forms zacetek

--- kme_project/kme/admin.py
+++ kme_project/kme/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from kme.models import Vlagatelj, Raziskava
+
+class VlagateljAdmin(admin.ModelAdmin):
+    fields = ['ime', 'priimek', 'sifra_arrs', 'email']
+
+class RaziskavaAdmin(admin.ModelAdmin):
+    list_display = ('naslov_raziskave_slo', 'sifra_raziskave', 'tip_raziskave', 'tip_raziskave_drugo')
+
+admin.site.register(Vlagatelj, VlagateljAdmin)
+admin.site.register(Raziskava, RaziskavaAdmin)
--- kme_project/kme/models.py
+++ kme_project/kme/models.py
@@ -2,19 +2,32 @@ from django.db import models
 
 # Create your models here.
 class Vlagatelj(models.Model):
-    ime = CharField(max_length=200)
-    priimek = CharField(max_length=200)
-    sifra_arrs = CharField(max_length=200, blank=True)
-    email = EmailField(max_length=254)
+    ime = models.CharField(max_length=200)
+    priimek = models.CharField(max_length=200)
+    sifra_arrs = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=254)
+
+    class Meta:
+        verbose_name_plural = 'vlagatelji'
+
+    def __str__(self):
+        return self.priimek
 
 class Raziskava(models.Model):
     TIP_RAZISKAVE_CHOICES = (
         ('DIPL', 'Diplomska naloga'),
         ('KLIN', 'Klinična farmakološka'),
         ('MP', 'Preverjanje medicinskega pripomočka'),
-        ('DRUGO', 'Drugo'),
+        ('DRUGO', 'Drugo')
+    )
 
-    naslov_raziskave_slo = TextField()
-    sifra_raziskave = CharField(max_length=200)
-    tip_raziskave = CharField(max_length=200, choices=TIP_RAZISKAVE_CHOICES)
-    tip_raziskave_drugo = CharField(max_length=200, blank=True)
+    naslov_raziskave_slo = models.TextField()
+    sifra_raziskave = models.CharField(max_length=200)
+    tip_raziskave = models.CharField(max_length=200, choices=TIP_RAZISKAVE_CHOICES)
+    tip_raziskave_drugo = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'raziskave'
+
+    def __str__(self):
+        return self.naslov_raziskave_slo
