1097e1912116359bc2b6f92cf23b1b2f3bcac50e
matonsjojc <matevz.podlipnik@gmail.com>
2017-06-06 21:50:51 +0200

    forms zacetek

--- kme_project/kme/forms.py
+++ kme_project/kme/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+
+from kme.models import Vlagatelj, Raziskava
+
+class VlagateljForm(forms.ModelForm):
+    ime = forms.CharField(max_length=200,
+                          help_text="Ime")
+    priimek = forms.CharField(max_length=200,
+                              help_text="Priimek")
+    sifra_arrs = forms.CharField(max_length=200,
+                                 help_text="ce obstaja")
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = Vlagatelj
+        fields = ('ime', 'priimek', 'sifra_arrs', 'email')
