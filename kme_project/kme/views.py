6275fb2cd5c7d7b55cff4676b0e4b735928797b3
matonsjojc <matevz.podlipnik@gmail.com>
2017-06-08 23:43:02 +0200

    index template kao ok.

--- kme_project/kme/views.py
+++ kme_project/kme/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
